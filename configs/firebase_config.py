afaaf46ab06bb213268fa6415b7882acaac7d52d
benalioune <benalioune6@gmail.com>
2024-04-15 15:24:21 +0200

    init project

---
+++ configs/firebase_config.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyCiefwkMVpzEDDFMyeixp84WytVCapaCIo",
+  "authDomain": "todo-a0df3.firebaseapp.com",
+  "projectId": "todo-a0df3",
+  "databaseURL" : "https://todo-a0df3-default-rtdb.firebaseio.com/",
+  "storageBucket": "todo-a0df3.appspot.com",
+  "messagingSenderId": "109779650142",
+  "appId": "1:109779650142:web:5dce737f35e1e93440d6cf",
+  "measurementId": "G-5GHGZMJH9W"
+}
